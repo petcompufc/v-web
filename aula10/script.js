1c357003cf44f760e886442811177f06ad0e3b51
Gabriel Freire <gabrielfvale@gmail.com>
2019-05-05 18:27:29 -0300

    Aula 10: Resolução do exercício

--- aula10/script.js
+++ aula10/script.js
@@ -8,4 +8,16 @@ const updateProfile = () => {
   // https://reqres.in/api/users/${id}
   // document.getElementById("nome").innerHTML = `${nome} ${sobrenome}`;
   // document.getElementById("avatar").src = avatar;
+
+  fetch('https://reqres.in/api/users/' + getRandomInt(1, 12))
+  .then(response => response.json())
+  .then(json => {
+    let data = json.data;
+    
+    document.getElementById("nome")
+    .innerHTML = `${data.first_name} ${data.last_name}`;
+
+    document.getElementById("avatar")
+    .src = data.avatar;
+  })
 }
